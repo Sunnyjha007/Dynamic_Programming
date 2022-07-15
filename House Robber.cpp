a38e6eae17882bdf20d2972cf942ad0ceb94c289
sunny jha <78363637+Sunnyjha007@users.noreply.github.com>
2022-07-15 12:32:08 +0530

    Update House Robber.cpp

--- House Robber.cpp
+++ House Robber.cpp
@@ -1,3 +1,32 @@
+//recursion
+ int solve(vector<int>& nums,int n){
+        if(n==0)return nums[n];
+        if(n<0)return 0;
+        int pick = solve(nums,n-2) + nums[n];
+        int non_pick = solve(nums,n-1);
+        return max(pick,non_pick);
+        
+    }
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        return solve(nums,n-1);
+}
+//memo
+int solve(vector<int>& nums,int n,vector<int>& dp){
+        if(n==0)return nums[n];
+        if(n<0)return 0;
+        if(dp[n]!=-1)return dp[n];
+        int pick = solve(nums,n-2,dp) + nums[n];
+        int non_pick = solve(nums,n-1,dp);
+        return dp[n] =  max(pick,non_pick);
+        
+    }
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n+1,-1);
+        return solve(nums,n-1,dp);
+}
+//bottom up
 int rob(vector<int>& nums) {
         int n = nums.size();
         if(n==1)return nums[0];
@@ -8,4 +37,18 @@ int rob(vector<int>& nums) {
             dp[i] = max(dp[i-1],dp[i-2]+nums[i]);
         }
         return dp[n-1];
+}
+//space optimization
+int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(n==1)return nums[0];
+        if(n==2)return max(nums[0],nums[1]);
+        int prev1 = nums[0],prev2 = max(nums[0],nums[1]);
+        int ans = prev2;
+        for(int i=2;i<n;i++){
+            ans = max(prev2,prev1+nums[i]);
+            prev1 = prev2;
+            prev2 = ans;
+        }
+        return ans;
 }
